1bd67c3f0c7a1b4dabbb5b9c2ced676cb3714735
Kenji Maillard <kenji@maillard.blue>
2019-11-27 10:38:49 +0100

    explicit formula for demonic relation interpretation

--- theories/Relational/RelationalNDD.v
+++ theories/Relational/RelationalNDD.v
@@ -42,4 +42,8 @@ Section NDDs.
     ⊨ pick_set ≈ ret a2 [{ demonic_left_rule_w a2 }].
   Proof. cbv ; intuition. destruct H0; destruct x; assumption. Qed.
 
+  Lemma demonicInterpretationExplicitFormula A1 A2 c1 c2 (post : A1 × A2 -> SProp):
+    ((θforallrel ⟨A1, A2⟩)∙1 ⟨c1,c2⟩)∙1 post s<-> forall a1 a2, c1 a1 -> c2 a2 -> post ⟨a1,a2⟩.
+  Proof. cbv; intuition. Qed.
+
 End NDDs.
