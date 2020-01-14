f6fd2db3b21278f093d40916380c33bf891aaa02
antoine_vanmuylder <antvanmul@hotmail.com>
2020-01-14 19:05:20 +0100

    SPropBase SProp-patched

--- theories/Mon/sprop/SPropBase.v
+++ theories/Mon/sprop/SPropBase.v
@@ -7,6 +7,12 @@ From Mon Require Import Base.
 From mathcomp Require Import ssreflect.
 (* Conflicts with Coq.Utf8 and provides the same things *)
 
+From Coq Require ClassicalFacts.
+Locate proof_irrelevance.
+
+Axiom ax_proof_irrel : ClassicalFacts.proof_irrelevance.
+
+
 Set Primitive Projections.
 
 (********************************************************************)
@@ -19,6 +25,26 @@ Set Primitive Projections.
 (* Create HintDb sprop discriminated. *)
 
 (** SProp type formers and notations not present in the stdlib *)
+
+(** sprop sigma types*)
+Module Redefined_sprop_constructs.
+
+Definition Ssig := sig.
+Arguments Ssig {A} P.
+Definition Sexists := exist.
+Arguments Sexists {A} P x e.
+Definition Spr1 {A : Type} {P : A -> Prop} (inst : Ssig P) :=
+  (let (w,_) := inst in w).
+Definition Spr2 {A : Type} {P : A -> Prop} (inst : Ssig P)
+: P (Spr1 inst ). destruct inst. simpl. assumption. Defined.
+About eq_ind.
+Definition eq_sind := eq_ind.
+Arguments eq_sind {A}.
+
+Record Box (A:Prop) : Prop := box { unbox : A }.
+
+End Redefined_sprop_constructs.
+Export Redefined_sprop_constructs.
 
 (** Equality in SProp *)
 
@@ -137,10 +163,8 @@ Module SPropNotations.
   Notation "x ≡ y" := (@sEq _ x y) (at level 70, no associativity).
 (*the following comes from Logic.StrictProp , coq 8.10 *)
 (*Record Ssig {A:Type} (P:A->SProp) := Sexists { Spr1 : A; Spr2 : P Spr1 }.*)
-Print sigT.
-  Notation "{ x : A ≫ P }" := (@sigT A (fun x => P)) (x at level 99).
-(*old sprop notation 
-  Notation "{ x : A ≫ P }" := (@Ssig A (fun x => P)) (x at level 99). *)
+
+  Notation "{ x : A ≫ P }" := (@Ssig A (fun x => P)) (x at level 99).
   Notation "s∃ x .. y , p" :=
     (Ex (fun x => .. (Ex (fun y => p )) .. ))
       (at level 200, x binder, y binder, right associativity).
@@ -156,23 +180,20 @@ Print sigT.
   Notation "n s<= m" := (Sle n m) (at level 70).
   Notation "n s< m" := (Sle (S n)  m) (at level 70).
 
-(*the following comes from Logic.StrictProp , coq 8.10 *)
-(*Record Ssig {A:Type} (P:A->SProp) := Sexists { Spr1 : A; Spr2 : P Spr1 }.*)
-Print sigT.
-  Notation "⦑ t ⦒" := (existT _ t _).
-(* old notation 
-  Notation "⦑ t ⦒" := (Sexists _ t _). *)
-Print sigT.
-  Notation " x ∙1" := (let (w,_) := x in w) (at level 2).
-  Notation " x ∙2" := (let (_,e) := x in e) (at level 2).
-(* old notations
+  Notation "⦑ t ⦒" := (Sexists _ t _).
+
   Notation " x ∙1" := (Spr1 x) (at level 2).
-  Notation " x ∙2" := (Spr2 x) (at level 2). *)
+  Notation " x ∙2" := (Spr2 x) (at level 2).
 End SPropNotations.
 
 
 Section sEqLemmas.
+
   Import SPropNotations.
+
+  Lemma sEq_to_eq {A} {x y: A} (H : x ≡ y) : x = y.
+  compute in H. assumption. Qed.
+
   Definition sEq_sym {A} {x y : A} (H : x ≡ y) : y ≡ x.
     compute in H. rewrite H. compute. reflexivity.
   Defined.
@@ -182,7 +203,12 @@ Section sEqLemmas.
   Defined.
 
   Lemma eq_to_sEq {A} {x y: A} (H : x = y) : x ≡ y.
-  Proof. induction H ; reflexivity. Qed.
+  Proof. induction H ; reflexivity. Defined.
+
+(*
+Coercion sEq_to_eq : sEq >-> eq.
+Coercion eq_to_sEq : eq >-> sEq.
+*)
 
   Definition f_sEqual {A B} (f : A -> B) {x y} (H : x ≡ y) : f x ≡ f y.
   Proof. induction H ; constructor. Qed.
@@ -200,15 +226,15 @@ Section sEqLemmas.
     (forall a a' (H : a = a'), p a a' (eq_to_sEq H)) ->
     forall (a a' : A) (H:a ≡ a'), p a a' H.
   Proof.
-    intros f a a' H. compute in H. Set Printing All. 
-    compute in p. pose ff := (f _ _ H). compute in ff.
-
-apply (f a a' H).
-generalize (f a a'). induction H.
-    intros f' ; apply (f' eq_refl).
+    intros f a a' H.
+    have hintUnif :eq_to_sEq H = H.
+      compute in H. compute. destruct H. reflexivity.
+    rewrite -hintUnif. apply f.
   Qed.
+  
 End sEqLemmas.
 
+About sEq.
 Ltac f_sEqual :=
     match goal with
     | [|- sEq (?f1 ?x1) (?f2 ?x2)] =>
@@ -224,24 +250,26 @@ Ltac subst_sEq :=
 
 Section SsigLemmas.
 
-  Lemma Ssig_eq {A} (P : A -> SProp) :
+  Lemma Ssig_eq {A} (P : A -> Prop) :
     forall (mx my : Ssig P), Spr1 mx = Spr1 my -> mx = my.
   Proof.
     intros [cx ex] [cy ey]. simpl.
-    induction 1. reflexivity.
+    induction 1.
+    have hintUnif : ex = ey.
+      by apply ax_proof_irrel.
+    rewrite hintUnif. reflexivity.
   Defined.
 
   Lemma transport_Ssig :
-    forall {A B} (F : B -> A -> SProp) {x y} h z,
+    forall {A B} (F : B -> A -> Prop) {x y} h z,
       eq_rect x (fun x => Ssig (fun b => F b x)) z y h
       = Sexists _ (Spr1 z) (@eq_sind A x (F (Spr1 z)) (Spr2 z) y h).
   Proof.
     intros.
-    dependent inversion h.
-    reflexivity.
+    dependent inversion h. compute. destruct z. reflexivity.
   Qed.
 
-  Lemma eq_above_Ssig {A B} (F : B -> A -> SProp)
+  Lemma eq_above_Ssig {A B} (F : B -> A -> Prop)
         (G := fun x => Ssig (fun b => F b x)) {x1 x2 : A} {h : x1 = x2}
         {z1 : G x1} {z2 : G x2} :
     Spr1 z1 = Spr1 z2 -> z1 =⟨ h ⟩ z2.
@@ -290,7 +318,7 @@ Module SPropAxioms.
   (** Propositional extensionality *)
   Import SPropNotations.
 
-  Axiom sprop_ext : forall {p q : SProp}, p = q <-> Box (sand (p -> q) (q -> p)).
+  Axiom sprop_ext : forall {p q : Prop}, p = q <-> Box (sand (p -> q) (q -> p)).
 
 
   (** Functional Extensionality *)
@@ -305,7 +333,7 @@ Module SPropAxioms.
       [ |- ?X ≡ ?Y ] => apply (@funext_sprop _ _ X Y) ; intros x
     end.
 
-  Axiom funext_sprop' : forall (A : SProp) (B : A -> Type) (f g : forall x : A, B x),
+  Axiom funext_sprop' : forall (A : Prop) (B : A -> Type) (f g : forall x : A, B x),
       (forall x : A, f x ≡ g x) -> f ≡ g.
 
   Tactic Notation "funext_s" simple_intropattern(x) :=
@@ -319,9 +347,9 @@ End SPropAxioms.
 Section SPropExamplesAndCounterExamples.
 
   (* As indicated in the ceps/37 there is no cumulativity *)
-  Goal SProp -> Type.
+  Goal Prop -> Type.
   Proof.
-    intro x ; exact x. Fail Qed. (* Universe inconsistency *)
+    intro x ; exact x. (* Universe inconsistency *)
     Restart.
     intro x ; exact (Box x). (* But it works if wrapped (as it should) *)
   Qed.
@@ -330,20 +358,23 @@ Section SPropExamplesAndCounterExamples.
   Record hprop := { ty : Type ; is_hprop : forall (x y : ty), x = y}.
 
   (* SProps should be subsingleton (definitionally) *)
-  Definition sprop_to_hprop (P : SProp) : hprop.
+  Definition sprop_to_hprop (P : Prop) : hprop.
   Proof.
     (* But trying directly, it fails (P is accepted in Type though...) *)
     exists P. intros. Fail reflexivity.
     Restart.
     (* However it works if we wrap the SProp (but that's quite unconvenient) *)
-    exists (Box P); intros [?] [?];  reflexivity.
+    exists (Box P); intros [?] [?].
+    have hintUnif : unbox0 = unbox1.
+      by apply ax_proof_irrel.
+    rewrite hintUnif. reflexivity.
   Qed.
 
   (* Here the problem is more visible, P is in SProp, not in Type so the equality is not well formed *)
-  Fail Check forall (P : SProp) (x y : P), x = y.
+  Fail Fail Check forall (P : Prop) (x y : P), x = y.
 
   (* The goal does not typecheck, but Coq still allows me to try to prove it ??? *)
-  Goal forall (P : SProp) (x y : P), x = y. Proof. admit. Fail Admitted.
+  Goal forall (P : Prop) (x y : P), x = y. Proof. admit. Fail Fail Admitted.
   Abort All.
   
 End SPropExamplesAndCounterExamples.
