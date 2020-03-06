70ed9faee8cba81479ae7f8dd3ba7d8f75df0ab0
antoine_vanmuylder <antvanmul@hotmail.com>
2020-03-06 17:20:51 +0100

    proof irrelevance added to the list of axioms + SDistr nearly finished

--- theories/Crypt/Axioms.v
+++ theories/Crypt/Axioms.v
@@ -1,8 +1,11 @@
 From mathcomp Require Import all_ssreflect all_algebra reals.
 
+
 Import ssrnum.Num.Theory.
 Local Open Scope ring_scope.
 
 Parameter (R:realType).
 Parameter  (absord : R -> R -> bool). 
 Parameter (unlock_absord : absord = (fun x y : R => x <= y)).
+
+From Coq Require Export Logic.ProofIrrelevance.
