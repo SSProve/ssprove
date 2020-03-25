70c343ffd9fd88f8b2c79aa66be2b8a0ab70bcfc
antoine_vanmuylder <antvanmul@hotmail.com>
2020-03-25 17:37:35 +0100

    Prop vs SProp equivalence

--- theories/Crypt/Axioms.v
+++ theories/Crypt/Axioms.v
@@ -10,3 +10,21 @@ Parameter (unlock_absord : absord = (fun x y : R => x <= y)).
 
 From Coq Require Export Logic.ProofIrrelevance.
 From Coq Require Export FunctionalExtensionality.
+
+(*now an equivalence between SProp and Prop. An equivalence of
+truth collections.*)
+Parameter (Prop2SProp : Prop -> SProp).
+Parameter (SProp2Prop : SProp -> Prop).
+
+Axiom (PSP_retr :  forall Q : Prop , SProp2Prop (Prop2SProp Q) = Q ).
+Axiom (PSP_sect : forall Q' : SProp, Prop2SProp( SProp2Prop Q' ) = Q').
+
+Axiom (Prop2SProp_truthMorphism_leftRight :
+forall Q : Prop, Q -> (Prop2SProp Q)).
+Axiom (Prop2SProp_truthMorphism_rightLeft :
+forall Q : Prop, (Prop2SProp Q) -> Q).
+
+Axiom (SProp2Prop_truthMorphism_leftRight :
+forall Q' : SProp, Q' -> SProp2Prop Q').
+Axiom (SProp2Prop_truthMorphism_rightLeft :
+forall Q' : SProp, SProp2Prop Q' -> Q').
