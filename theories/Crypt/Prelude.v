29d332f548d3466bd0ce37dbd7a8150ccc61ec9c
Théo Winterhalter <theo.winterhalter@gmail.com>
2020-11-17 14:17:01 +0100

    Add Prelude to Crypt for basic notations

---
+++ theories/Crypt/Prelude.v
@@ -0,0 +1,24 @@
+(* Global utility *)
+(* Stolen from MetaCoq *)
+
+
+(* Simple products *)
+
+Notation "x × y" := (prod x y ) (at level 80, right associativity).
+
+
+(* Dependent sums *)
+
+(* Use \sum to input ∑ in Company Coq (it is not a sigma Σ). *)
+Notation "'∑' x .. y , p" := (sigT (fun x => .. (sigT (fun y => p%type)) ..))
+  (at level 200, x binder, right associativity,
+   format "'[' '∑'  '/  ' x  ..  y ,  '/  ' p ']'")
+  : type_scope.
+
+Notation "( x ; y )" := (@existT _ _ x y).
+Notation "( x ; y ; z )" := (x ; ( y ; z)).
+Notation "( x ; y ; z ; t )" := (x ; ( y ; (z ; t))).
+Notation "( x ; y ; z ; t ; u )" := (x ; ( y ; (z ; (t ; u)))).
+Notation "( x ; y ; z ; t ; u ; v )" := (x ; ( y ; (z ; (t ; (u ; v))))).
+Notation "x .π1" := (@projT1 _ _ x) (at level 3, format "x '.π1'").
+Notation "x .π2" := (@projT2 _ _ x) (at level 3, format "x '.π2'").
