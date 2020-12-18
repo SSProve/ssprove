d42cc076c35d23e16d317a6dd05e13550982f9d0
antoine_vanmuylder <antvanmul@hotmail.com>
2020-12-18 14:53:15 +0100

    Start cleaning and commenting

--- theories/Crypt/Axioms.v
+++ theories/Crypt/Axioms.v
@@ -1,38 +1,20 @@
 From mathcomp Require Import all_ssreflect all_algebra reals boolp.
 
-
-Import ssrnum.Num.Theory.
 Local Open Scope ring_scope.
 
-Parameter (R:realType).
-Parameter  (absord : R -> R -> bool). 
-Parameter (unlock_absord : absord = (fun x y : R => x <= y)).
+(*
+In this file we assume several axioms.
+*)
 
-(* From Coq Require Export Logic.ProofIrrelevance. *)
-(* We can instead obtain proof_irrelevance from boolp in mathcomp: *)
+Parameter (R:realType).
+Parameter  (absord : R -> R -> bool).  (*sealed order on real numbers*)
+Parameter (unlock_absord : absord = (fun x y : R => x <= y)). (*unseal with this*)
+
+
+(* We can obtain proof_irrelevance from boolp in mathcomp: *)
 Definition proof_irrelevance : forall (P:Prop) (p1 p2:P), p1 = p2 := Prop_irrelevance.
 From Coq Require Export FunctionalExtensionality.
 
-(*now an equivalence between SProp and Prop. An equivalence of
-truth collections.*)
-
-(*
-Parameter (Prop2SProp : Prop -> SProp).
-Parameter (SProp2Prop : SProp -> Prop).
-
-Axiom (PSP_retr :  forall Q : Prop , SProp2Prop (Prop2SProp Q) = Q ).
-Axiom (PSP_sect : forall Q' : SProp, Prop2SProp( SProp2Prop Q' ) = Q').
-
-Axiom (Prop2SProp_truthMorphism_leftRight :
-forall Q : Prop, Q -> (Prop2SProp Q)).
-Axiom (Prop2SProp_truthMorphism_rightLeft :
-forall Q : Prop, (Prop2SProp Q) -> Q).
-
-Axiom (SProp2Prop_truthMorphism_leftRight :
-forall Q' : SProp, Q' -> SProp2Prop Q').
-Axiom (SProp2Prop_truthMorphism_rightLeft :
-forall Q' : SProp, SProp2Prop Q' -> Q').
-*)
 
 (* The following are not axioms per se, but come from boolp using axioms *)
 Local Definition func (A B : Type) (R : A -> B -> Prop) :
