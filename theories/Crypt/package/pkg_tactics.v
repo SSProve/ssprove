d2ef917bf2fd50f6762995c6bcba80c07ea79bf7
Théo Winterhalter <theo.winterhalter@gmail.com>
2021-01-09 13:34:51 +0100

    Move experimental setoid rewrite away

--- theories/Crypt/package/pkg_tactics.v
+++ theories/Crypt/package/pkg_tactics.v
@@ -30,10 +30,12 @@
 
 **)
 
-From mathcomp Require Import ssreflect ssrbool eqtype seq.
+From mathcomp Require Import ssreflect ssrbool eqtype seq eqtype choice.
 From extructures Require Import ord fset.
 From Crypt Require Export pkg_core_definition pkg_composition pkg_notation
   RulesStateProb.
+From Coq Require Import FunctionalExtensionality
+  Setoids.Setoid Classes.Morphisms.
 
 Require Equations.Prop.DepElim.
 
@@ -79,5 +81,35 @@ Module PackageTactics (π : RulesParam).
 
   Tactic Notation "program" "fold" :=
     program_fold.
+
+  (* With the following, one can rewrite under λ with setoid_rewrite *)
+
+  Instance pointwise_eq_ext {A B : Type} {RB : relation B} (sb : subrelation RB eq) :
+    subrelation (pointwise_relation A RB) eq.
+  Proof.
+    intros f g Hfg.
+    apply functional_extensionality.
+    intro x. apply sb. apply Hfg.
+  Qed.
+
+  (** Folding under binders with setoid_rewrite *)
+
+  (* TODO Add Proper for other contexts *)
+
+  Instance sampler_iff_morphism L I (A : choiceType) o :
+    Proper (pointwise_relation (Arit o) eq ==> eq) (@sampler L I A o).
+  Proof.
+    simpl_relation.
+    f_equal. apply functional_extensionality. auto.
+  Qed.
+
+  Opaque sampler getr putr ret opr.
+
+  Tactic Notation "program" "setoid" "fold" :=
+    try setoid_rewrite fold_putr ;
+    try setoid_rewrite fold_getr ;
+    try setoid_rewrite fold_opr ;
+    try setoid_rewrite fold_sampler ;
+    try setoid_rewrite fold_ret.
 
 End PackageTactics.
