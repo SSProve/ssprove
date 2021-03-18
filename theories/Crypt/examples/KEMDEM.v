40a8d126d4edb600122ee686d5ae94d5239132b0
Théo Winterhalter <theo.winterhalter@gmail.com>
2021-03-18 12:57:27 +0100

    Update KEM DEM to new setting

--- theories/Crypt/examples/KEMDEM.v
+++ theories/Crypt/examples/KEMDEM.v
@@ -11,6 +11,9 @@ Set Warnings "notation-overridden,ambiguous-paths,notation-incompatible-format".
 From Crypt Require Import Axioms ChoiceAsOrd SubDistr Couplings
   UniformDistrLemmas FreeProbProg Theta_dens RulesStateProb UniformStateProb
   pkg_composition pkg_rhl pkg_notation Package Prelude pkg_notation.
+Set Warnings "-custom-entry-overriden".
+From Crypt Require Import package_instance.
+Set Warnings "custom-entry-overriden".
 
 From Coq Require Import Utf8 Lia.
 From extructures Require Import ord fset fmap.
@@ -28,58 +31,11 @@ Import Num.Def.
 Import Num.Theory.
 Import mc_1_10.Num.Theory.
 
-#[local] Open Scope ring_scope.
-
-Inductive probEmpty : Type → Type := .
-
-Module genparam <: RulesParam.
-
-  Definition probE : Type → Type := probEmpty.
-
-  Definition rel_choiceTypes : Type := chUniverse.
-
-  Definition chEmb : rel_choiceTypes → choiceType := chElement.
-
-  Definition prob_handler : ∀ T : choiceType, probE T → SDistr T.
-  Proof.
-    intros T v. inversion v.
-  Defined.
-
-End genparam.
-
-(* TODO *)
-Variant Index := i_key (l : nat) `{Positive l}.
-
-Module Uparam <: UniformParameters.
-
-  Definition Index : Type := Index.
-
-  Definition i0 : Index := i_key 1.
-
-  Definition fin_family : Index → finType :=
-    λ i,
-      match i with
-      | i_key l pl => [finType of chFin (mkpos l)]
-      end.
-
-  Definition F_w0 : ∀ i, fin_family i.
-  Proof.
-    unfold fin_family.
-    case.
-    intros. simpl. exists 0%N. auto.
-  Defined.
-
-End Uparam.
-
-Module Rules := DerivedRulesUniform genparam Uparam.
-Import Rules.
-
-Module Package := Package_Make myparamU.
-Import Package.
 Import PackageNotation.
 
-Definition U (i : Index) : Op :=
-  existT _ (inl (inl i)) (inl (Uni_W i)).
+#[local] Open Scope ring_scope.
+
+Definition i_key (l : nat) := l.
 
 Definition GEN := 0%N.
 Definition SET := 1%N.
