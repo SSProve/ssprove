58320c5cef864dc16e3be9ebf7354e8363a028e0
Théo Winterhalter <theo.winterhalter@gmail.com>
2021-04-15 10:18:01 +0200

    Write down most interfaces for KEM-DEM

--- theories/Crypt/examples/KEMDEM.v
+++ theories/Crypt/examples/KEMDEM.v
@@ -33,29 +33,6 @@ Import PackageNotation.
 #[local] Open Scope ring_scope.
 #[local] Open Scope package_scope.
 
-(** Procedure names
-
-  All in one place so it's easier.
-
-*)
-
-(* KEY *)
-Definition GEN := 0%N.
-Definition SET := 1%N.
-Definition GET := 2%N.
-
-(* MOD-CCA *)
-Definition PKGEN := 3%N.
-Definition PKENC := 4%N.
-Definition PKDEC := 5%N.
-
-(** KEY Package *)
-
-(* TODO Section for length?  *)
-Definition i_key (l : nat) := l.
-
-Definition key n `{Positive n} : Location := ('option ('fin n) ; 0%N).
-
 (* TODO MOVE *)
 (* TODO Same as finmap.oextract but with a better name? *)
 Definition getSome {A} (o : option A) :
@@ -66,43 +43,139 @@ Proof.
   assumption.
 Defined.
 
-Definition KEY n `{Positive n} :
-  package
-    (fset [:: key n ])
-    [interface]
-    [interface
-      val #[ GEN ] : 'unit → 'unit ;
-      val #[ SET ] : ('fin n) → 'unit ;
-      val #[ GET ] : 'unit → 'fin n
-    ] :=
-  [package
-    def #[ GEN ] (_ : 'unit) : 'unit {
-      k ← get (key n) ;;
-      assert (k == None) ;;
-      k ← sample uniform (i_key n) ;;
-      put (key n) := Some k ;;
-      ret Datatypes.tt
-    } ;
-    def #[ SET ] (k : ('fin n)) : 'unit {
-      k' ← get (key n) ;;
-      assert (k' == None) ;;
-      put (key n) := Some k ;;
-      ret Datatypes.tt
-    } ;
-    def #[ GET ] (_ : 'unit) : ('fin n) {
-      k ← get (key n) ;;
-      #assert (isSome k) as kSome ;;
-      @ret ('fin n) (getSome k kSome)
-    }
-  ].
+Section KEMDEM.
 
-(** MOD-CCA *)
+  (** In the SSP paper, we have λ.
+      key_length would 2^λ because we do not use bitstrings.
+  *)
+  Context (key_length : nat) `{Positive key_length}.
 
-Definition MODCCA l `{Positive l} :
-  package
-    (fset [:: (* TODO *) ])
-    [interface] (* TODO Import KEM and DEM *)
-    [interface
-      val #[ PKGEN ] : 'unit → ('fin l) × ('fin l)
+  Context (plain_length : nat) `{Positive plain_length}.
+
+  (** In the paper, the following are functions of |m|, here we assume |m|
+      is constant: plain_length.
+  *)
+  Context (clen : nat) `{Positive clen}.
+  Context (elen : nat) `{Positive elen}.
+
+  (** Types *)
+  Notation "'key" := ('fin key_length) (in custom pack_type at level 2).
+  Notation "'key" := ('fin key_length) (at level 2) : package_scope.
+
+  Notation "'plain" := ('fin plain_length) (in custom pack_type at level 2).
+  Notation "'plain" := ('fin plain_length) (at level 2) : package_scope.
+
+  Notation "'clen" := ('fin clen) (in custom pack_type at level 2).
+  Notation "'clen" := ('fin clen) (at level 2) : package_scope.
+
+  Notation "'elen" := ('fin elen) (in custom pack_type at level 2).
+  Notation "'elen" := ('fin elen) (at level 2) : package_scope.
+
+  (** Procedure names *)
+
+  (* KEY *)
+  Definition GEN := 0%N.
+  Definition SET := 1%N.
+  Definition GET := 2%N.
+
+  (* KEM-CCA *)
+  Definition KEMGEN := 6%N.
+  Definition ENCAP := 7%N.
+  Definition DECAP := 8%N.
+
+  (* DEM-CCA *)
+  Definition ENC := 9%N.
+  Definition DEC := 10%N.
+
+  (* PKE-CCA / MOD-CCA *)
+  Definition PKGEN := 3%N.
+  Definition PKENC := 4%N.
+  Definition PKDEC := 5%N.
+
+  (** Memory locations *)
+  Definition key : Location := ('option ('fin key_length) ; 0%N).
+
+  (** Uniform distributions *)
+  Definition i_key := key_length.
+
+  (** KEY Package *)
+
+  Definition KEY :
+    package
+      (fset [:: key ])
+      [interface]
+      [interface
+        val #[ GEN ] : 'unit → 'unit ;
+        val #[ SET ] : 'key → 'unit ;
+        val #[ GET ] : 'unit → 'key
+      ] :=
+    [package
+      def #[ GEN ] (_ : 'unit) : 'unit {
+        k ← get key ;;
+        assert (k == None) ;;
+        k ← sample uniform i_key ;;
+        put key := Some k ;;
+        ret Datatypes.tt
+      } ;
+      def #[ SET ] (k : 'key) : 'unit {
+        k' ← get key ;;
+        assert (k' == None) ;;
+        put key := Some k ;;
+        ret Datatypes.tt
+      } ;
+      def #[ GET ] (_ : 'unit) : 'key {
+        k ← get key ;;
+        #assert (isSome k) as kSome ;;
+        @ret 'key (getSome k kSome)
+      }
     ].
-Abort.
+
+  (** Assumed KEM game *)
+  (* TODO Not sure if I should do this, or simply assusme some code. *)
+  Definition KEM_export :=
+    [interface
+      val #[ KEMGEN ] : 'unit → 'key ;
+      val #[ ENCAP ] : 'unit → 'elen ;
+      val #[ DECAP ] : 'elen → 'key
+    ].
+
+  Context (KEM₀ : loc_package [interface val #[ SET ] : 'key → 'unit ] KEM_export).
+  Context (KEM₁ : loc_package [interface val #[ GEN ] : 'unit → 'unit ] KEM_export).
+
+  (** Assumed DEM game *)
+  Definition DEM_export :=
+    [interface
+      val #[ ENC ] : 'plain → 'clen ;
+      val #[ DEC ] : 'clen → 'plain
+    ].
+
+  Context (DEM₀ : loc_package [interface val #[ GET ] : 'unit → 'key ] DEM_export).
+  Context (DEM₁ : loc_package [interface val #[ GET ] : 'unit → 'key ] DEM_export).
+
+  (** PKE-CCA *)
+
+  Definition PKE_CCA :
+    package
+      (fset [:: (* TODO *) ])
+      [interface (* TODO *) ]
+      [interface
+        val #[ PKGEN ] : 'unit → 'key ;
+        val #[ PKENC ] : 'plain → 'clen ;
+        val #[ PKDEC ] : 'elen × 'clen → 'plain
+      ].
+  Abort.
+
+  (** MOD-CCA *)
+
+  Definition MOD_CCA :
+    package
+      (fset [:: (* TODO *) ])
+      [interface (* TODO *) ]
+      [interface
+        val #[ PKGEN ] : 'unit → 'key ;
+        val #[ PKENC ] : 'plain → 'clen ;
+        val #[ PKDEC ] : 'elen × 'clen → 'plain
+      ].
+  Abort.
+
+End KEMDEM.
