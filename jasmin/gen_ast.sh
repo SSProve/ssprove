af42cd1d1dfbeea56a409072ad5477cbdca4dd5d
Philipp G. Haselwarter <philipp@haselwarter.org>
2022-03-23 18:17:25 +0100

    update scripts

--- jasmin/gen_ast.sh
+++ jasmin/gen_ast.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# set path to jasminc.byte on command line by invoking the script with
+# JASMINC=... ./gen_ast.sh foo.jazz
+JASMINC=${JASMINC:-$(which jasminc.byte)}
+
 echo "open Format
 
 let print_vname (fmt : formatter) (t : Obj.t) =
@@ -22,13 +26,13 @@ Local Open Scope string.
 
 Definition ${name} :=" > $name/$name.v
 
-(ocamldebug $(which jasminc.byte) <<EOF
+(ocamldebug $JASMINC <<EOF
 load_printer "print_vname.cmo"
 install_printer Print_vname.print_vname
 set arguments $name.jazz
 set print_length 10000
 set print_depth 10000
-break @Jasmin.Main_compiler 179
+break @Jasmin.Main_compiler 201
 run
 print cprog
 EOF
@@ -39,5 +43,3 @@ sed -i '9,$!d;$d' $name/$name.cprog
 sed 's/Jasmin\.[[:graph:]]*\.//g; s/Coq_//g ; s/=/:=/g ; s/{/{| /g ; s/}/ |}/g ; s/[[:graph:]]*\.[[:graph:]]*/"&"/g ; s/()/tt/g ;/./{H;$!d}; x ; :rename_balanced ; s/(\([^(),@]*\))/<<\1>>/g ; t rename_balanced ; :rename_pairs1 ; s/\([{([|,;][ \t\n]*([^(),]*\),/\1%/g; t rename_pairs1 ; :rename_pairs ; s/\([{([|,;][ \t\n]*\)(\([^(),]*\))/\1<<\2>>/g; t rename_balanced ; :rename_curries1 ; s/\([^{([|,;][ \t\n]*([^()]*\),/\1@/g; t rename_curries1; :rename_curries ; s/\([^{([|,;][ \t\n]*\)(\([^(),]*\))/\1++\2##/g; t rename_balanced; :uncurry ; s/\([^{([|,;][ \t\n]*++[^(),]*\)@\([ \t\n]*\)/\1##\2++/g ; t uncurry s/\([^{([|][ \t\n]*++[^(),]*\)@\([ \t\n]*\)/\1##\2++/g ; t uncurry ; s/<</(/g ; s/>>/)/g ; s/##/)/g ; s/++/(/g ; s/%/,/g ; s/@/,/g' $name/$name.cprog >> $name/$name.v
 
 echo -n "." >> $name/$name.v
-
-mv $name.jazz $name
