81b4c1a86a853b4b15e364b265d74955dadb674d
Benjamin Salling Hvass <bsh@eng.au.dk>
2022-03-25 12:07:48 +0100

    stated the theorem

--- theories/Jasmin/jasmin_translate.v
+++ theories/Jasmin/jasmin_translate.v
@@ -11,7 +11,6 @@ From Coq Require Import Utf8.
 From Crypt Require Import Prelude Package.
 Import PackageNotation.
 
-
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
@@ -118,9 +117,9 @@ Proof.
   exact (projT2 unsupported).
 Defined.
 
-Record fdef := { _ : typed_raw_function ; _ : {fset Location} ; _ : Interface ; _ : Interface }.
+Record fdef := { ffun : typed_raw_function ; locs : {fset Location} ; imp : Interface ; exp : Interface }.
 
-Definition translate_fundef (fd : _fun_decl extra_fun_t) : funname * fdef.
+Definition translate_fundef (fd : _ufun_decl (* extra_fun_t *)) : funname * fdef.
 Proof.
   destruct fd. destruct _f.
   split. 1: exact f.
@@ -135,9 +134,9 @@ Proof.
   - exact [interface].
 Defined.
 
-Fixpoint satisfies_globs (globs : glob_decls) : heap -> Prop.
+Fixpoint satisfies_globs (globs : glob_decls) : heap * heap -> Prop.
 Proof.
-  exact (fun x => False).
+  exact (fun '(x, y) => False).
 Defined.
 
 Fixpoint collect_globs (globs : glob_decls) : seq Location.
@@ -145,20 +144,59 @@ Proof.
   exact nil.
 Defined.
 
-Definition ssprove_prog {T} := seq (funname * T).
+Definition ssprove_prog := seq (funname * fdef).
 
-Definition translate_prog (p:expr.prog) : ssprove_prog :=
+Definition translate_prog (p:uprog) : ssprove_prog :=
   let globs := collect_globs (p_globs p) in
   let fds := map translate_fundef (p_funcs p) in
   fds.
+Print typed_raw_function.
+Check Interface.
+About rel_jdg.
+About package.
+Check value.
 
-(* Theorem translate_correct entries (p : prog) (fn : funname) m va m' vr rf : *)
-(*   fn \in entries → *)
-(*   sem.sem_call p m fn va m' vr → *)
-(*   let sp := (translate_prog p) in *)
-(*   get_fundef fn sp = Some rf → *)
-(*   { satisfies_globs (p_globs p) } rf (fix_types rf (translate_values va)) ~ ret vr *)
-(*                                   { λ (v1,s1) (v2,s2), v1 = v2} *)
-(* Proof. *)
+Definition type_of_val : value -> choice_type.
+Proof.
+  intros.
+  exact chUnit.
+Defined.
+
+Fixpoint lchtuple (ts:seq choice_type) : choice_type :=
+  match ts with
+  | [::]   => chUnit
+  | [::t1] => t1
+  | t1::ts => chProd t1 (lchtuple ts)
+  end.
+
+Definition get_fundef_ssp (sp : seq (funname * fdef)) (fn : funname) (dom cod : choice_type) : option (dom -> raw_code cod).
+Proof.
+  exact None.
+Defined.
+
+Definition translate_value : value -> ∑ (T: choice_type), T.
+Proof.
+  intros. exists chUnit. exact.
+Defined.
+
+(* Definition seq_prod ls := *)
+(*   map translate_value ls *)
+(*   foldr chProd ls *)
+
+Definition translate_values (vs : seq value) : lchtuple (map type_of_val vs).
+Proof. Admitted.
+
+Theorem translate_correct (p : expr.uprog) (fn : funname) m va m' vr f :
+  sem.sem_call p m fn va m' vr →
+  let sp := (translate_prog p) in
+  let dom := lchtuple (map type_of_val va) in
+  let cod := lchtuple (map type_of_val vr) in
+  get_fundef_ssp sp fn dom cod = Some f →
+  (* let f := ffun rf in *)
+  (* let (S, f) := f in *)
+  (* let (T, f) := f in *)
+  ⊢ ⦃ satisfies_globs (p_globs p) ⦄ f (translate_values va) ≈ ret (translate_values vr) ⦃ λ '(v1, s1) '(v2,s2), v1 = v2 ⦄.
+Proof.
+  Admitted.
 
 End Section.
