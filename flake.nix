6c3ecb6dd6b4682a9c83db00868a25e021f93912
Sebastian Ertel <sebastianertel@Sebastians-MacBook-Air.local>
2024-02-13 15:16:07 +0100

    Nix support

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  inputs = {
+    nixpkgs.url        = github:nixos/nixpkgs;
+    flake-utils.url    = github:numtide/flake-utils;
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+       rec {
+          mkDrv = { stdenv, which, coqPackages, coq } :
+            stdenv.mkDerivation {
+              pname = "ssprove";
+              version = "0.0.1";
+              src = ./.;
+              nativeBuildInputs = [ which coq.ocamlPackages.findlib ] ++
+                                  (with coqPackages; [
+                                    equations
+                                    mathcomp-analysis
+                                    mathcomp-ssreflect
+                                    extructures
+                                    deriving
+                                  ]);
+              buildInputs = [ coq ];
+            };
+
+          devShell =
+            let
+              args = {
+                inherit (pkgs) stdenv which;
+                coq = pkgs.coq_8_18;
+                coqPackages = pkgs.coqPackages_8_18.overrideScope
+                  (self: super: {
+                    mathcomp = super.mathcomp.override { version = "2.1.0"; };
+                    mathcomp-analysis = super.mathcomp-analysis.override { version = "1.0.0"; };
+                  });
+              };
+              ssprove' = mkDrv args;
+            in
+              pkgs.mkShell {
+                packages =
+                  (with pkgs; [ coq gnumake ])
+                  ++
+                  (with ssprove';  nativeBuildInputs);
+              };
+       });
+}
