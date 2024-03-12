4d4ee9a1bcc7509a9daf908fd4cb297beece9a02
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-03-12 16:18:02 +0100

    slight nix flake restructure

--- flake.nix
+++ flake.nix
@@ -4,12 +4,8 @@
     flake-utils.url    = github:numtide/flake-utils;
   };
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-       rec {
-         mkDrv = { stdenv, which, coqPackages, coq } :
+    let
+      mkDrv = { stdenv, which, coqPackages, coq } :
            let
              extructures' = coqPackages.extructures.override { version = "0.4.0"; };
            in
@@ -27,25 +23,30 @@
                                   ++ [extructures'];
               buildInputs = [ coq ];
             };
-
-          devShell =
-            let
-              args = {
-                inherit (pkgs) stdenv which;
-                coq = pkgs.coq_8_18;
-                coqPackages = pkgs.coqPackages_8_18.overrideScope
-                  (self: super: {
-                    mathcomp = super.mathcomp.override { version = "2.1.0"; };
-                    mathcomp-analysis = super.mathcomp-analysis.override { version = "1.0.0"; };
-                  });
-              };
-              ssprove' = mkDrv args;
-            in
-              pkgs.mkShell {
-                packages =
-                  (with pkgs; [ coq gnumake ])
-                  ++
-                  (with ssprove';  nativeBuildInputs);
-              };
-       });
+    in { inherit mkDrv; } //
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+         rec {
+            devShell =
+              let
+                args = {
+                  inherit (pkgs) stdenv which;
+                  coq = pkgs.coq_8_18;
+                  coqPackages = pkgs.coqPackages_8_18.overrideScope
+                    (self: super: {
+                      mathcomp = super.mathcomp.override { version = "2.1.0"; };
+                      mathcomp-analysis = super.mathcomp-analysis.override { version = "1.0.0"; };
+                    });
+                };
+                ssprove' = mkDrv args;
+              in
+                pkgs.mkShell {
+                  packages =
+                    (with pkgs; [ coq gnumake ])
+                    ++
+                    (with ssprove';  nativeBuildInputs);
+                };
+         });
 }
