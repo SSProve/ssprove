1882afa81a8f3379f302b49c65cfd98d122487ab
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-05-03 21:53:23 +0200

    forcing the job to load the most recent state of nixpkgs

---
+++ .nix/nixpkgs.nix
@@ -0,0 +1,4 @@
+fetchTarball {
+         url = https://github.com/NixOS/nixpkgs/archive/d121ce778b2609ae9e749a711295ffca013a82c4.tar.gz;
+         sha256 = "d121ce778b2609ae9e749a711295ffca013a82c4";
+       }
