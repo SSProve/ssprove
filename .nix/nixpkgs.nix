ed79bb2ec94378fe3cce606b3a09ca67ddbb192a
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-05-03 22:11:56 +0200

    after updating nixpkgs.

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-         url = https://github.com/NixOS/nixpkgs/archive/d121ce778b2609ae9e749a711295ffca013a82c4.tar.gz;
-         sha256 = "d121ce778b2609ae9e749a711295ffca013a82c4";
+         url = https://github.com/NixOS/nixpkgs/archive/045a097bde2100af7301e50632a02d0425031042.tar.gz;
+         sha256 = "1gw6xkyzhc08q93dkb6p2s0c8b7vhj9nq5amwn5146ag38cp0j3l";
        }
