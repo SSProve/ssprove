ec7ceb8007a33bbfa49a69946a1f1f4b4488ce85
Markus Krabbe Larsen <markus@krabbelarsen.dk>
2025-04-30 17:22:07 +0200

    Small optimization of simplify_linking

--- theories/Crypt/package/pkg_user_util.v
+++ theories/Crypt/package/pkg_user_util.v
@@ -100,11 +100,8 @@ Set Primitive Projections.
 
 Open Scope pack.
 
-Ltac simplify_linking := simpl; repeat (
-  (  (rewrite resolve_set)
-  || (rewrite resolve_link)
-  || (rewrite coerce_kleisliE)
-  ); simpl).
+Ltac simplify_linking := simpl; repeat
+  (rewrite (resolve_set, resolve_link, coerce_kleisliE); simpl).
 
 Ltac simplify_eq_rel m :=
   let id := fresh "id" in
