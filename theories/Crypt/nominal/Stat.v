16505bcadf4a4d16b560662d7667d0452b1ec6cf
Markus Krabbe Larsen <markus@krabbelarsen.dk>
2025-08-13 22:31:42 +0200

    Simplify stat proof.

--- theories/Crypt/nominal/Stat.v
+++ theories/Crypt/nominal/Stat.v
@@ -30,17 +30,38 @@ Context (op : Op).
 
 Definition IPICK := [interface [ 0%N ] : { 'unit ~> op.π1 }].
 
+Definition cell : Location := (0%N, 'option (op.π1)).
+
 Definition RAND : game IPICK :=
-  [package emptym ;
+  [package [fmap cell] ;
     [ 0%N ] : { 'unit ~> op.π1 } 'tt {
-      r ← sample op ;;
-      ret r
+      mr ← get cell ;;
+      match mr with
+      | Some r => ret r
+      | None =>
+        r ← sample op ;;
+        #put cell := Some r ;;
+        ret r
+      end
     } ].
 
 Definition PICK (x : op.π1) : game IPICK :=
   [package emptym ;
     [ 0%N ] : { 'unit ~> op.π1 } 'tt {
       ret x
+    } ].
+
+Definition CELL : package IPICK IPICK :=
+  [package [fmap cell ] ;
+    [ 0%N ] : { 'unit ~> op.π1 } 'tt {
+      mc ← get cell ;;
+      match mc with
+      | Some c => ret c
+      | None => 
+        r ← call [ 0%N ] : { 'unit ~> op.π1 } tt ;;
+        #put cell := Some r ;;
+        ret r
+      end
     } ].
 
 (*Definition ops := mkopsig 0%N 'unit ('fin #|F|).*)
@@ -93,32 +114,7 @@ Proof.
   rewrite H //.
 Qed.
 
-Inductive NCalls {A} : nat → raw_code A → heap → Prop :=
-| valid_ret :
-    ∀ x n h,
-      NCalls n (ret x) h
-
-| valid_opr :
-    ∀ (o : opsig) x k n h,
-      (∀ v, NCalls n (k v) h) →
-      NCalls (S n) (opr o x k) h
-
-| valid_getr :
-    ∀ l k n h,
-      NCalls n (k (get_heap h l)) h →
-      NCalls n (getr l k) h
-
-| valid_putr :
-    ∀ l v k n h,
-      NCalls n k (set_heap h l v) →
-      NCalls n (putr l v k) h
-
-| valid_sampler :
-    ∀ op k n h,
-      (∀ v, NCalls n (k v) h) →
-      NCalls n (pkg_core_definition.sampler op k) h
-.
-
+(*
 Lemma testing0 {T} {A : raw_code T} {M} {h} :
   NCalls 0 A h →
   Pr_code (code_link A M) h = Pr_code A h.
@@ -131,49 +127,113 @@ Proof.
     apply distr_ext.
     apply dlet_f_equal => //.
 Qed.
+ *)
+
+Lemma testing0 {LA} {T} {A : raw_code T} {f f' : op.π1} {h} :
+  fseparate LA [fmap cell] →
+  ValidCode LA IPICK A →
+  get_heap h cell = Some f' →
+  Pr_code (code_link A RAND) h
+  = Pr_code (code_link A (CELL ∘ PICK f)) h.
+Proof.
+  intros SEP VA.
+  move: h; induction (VA) => h H'.
+  - done.
+  - fmap_invert H.
+    destruct x.
+    cbn [code_link].
+    repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]).
+    cbn [bind code_link].
+    rewrite 2!Pr_code_get H'.
+    cbn [code_link bind].
+    by apply H1.
+  - cbn[code_link].
+    rewrite 2!Pr_code_get.
+    by apply H1.
+  - cbn [code_link].
+    rewrite 2!Pr_code_put.
+    erewrite IHv => //.
+    rewrite get_set_heap_neq //.
+    apply fhas_in in H.
+    destruct SEP as [SEP].
+    move: SEP => /fdisjointP.
+    intros H''.
+    specialize (H'' _ H).
+    rewrite domm_set domm0 in H''.
+    apply /negP.
+    move=> /eqP H'''.
+    rewrite H''' in H''.
+    rewrite in_fsetU in_fset1 eq_refl // in H''.
+  - cbn [code_link].
+    rewrite 2!Pr_code_sample.
+    apply distr_ext.
+    apply dlet_f_equal => x.
+    by apply H0.
+Qed.
 
 Lemma testing1 {LA} {T} {A : raw_code T} {h} :
+  fseparate LA [fmap cell] →
   LosslessOp op →
-  NCalls 1 A h →
   ValidCode LA IPICK A →
   Pr_code (code_link A RAND) h
-    = \dlet_(f <- op.π2) Pr_code (code_link A (PICK f)) h.
+    = \dlet_(f <- op.π2) Pr_code (code_link A (CELL ∘ PICK f)) h.
 Proof.
-  intros LL NC VA.
+  intros SEP LL VA.
   apply distr_ext.
-  move: h NC; induction VA.
-  - intros h NC y.
+  move: h; induction VA.
+  - intros h y.
     rewrite Pr_code_ret.
     rewrite dletC.
     rewrite pr_predT.
     rewrite LL.
     rewrite GRing.mul1r //.
-  - intros h NC y.
+  - intros h y.
     fmap_invert H.
     cbn [code_link].
     under dlet_f_equal.
     1: intros ?; repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]); over.
     repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]).
     destruct x.
+    cbn [bind code_link].
+    rewrite Pr_code_get.
+    destruct (get_heap h cell) eqn:E.
+    { cbn [code_link bind].
+      under dlet_f_equal. {
+        intros x.
+        rewrite Pr_code_get E.
+        rewrite -(testing0 SEP (H0 _) E).
+        over.
+      }
+      cbn [bind].
+      rewrite dletC.
+      rewrite pr_predT.
+      rewrite LL.
+      rewrite GRing.mul1r //.
+    }
     rewrite Pr_code_sample.
     apply dlet_f_equal => x.
-    cbn [bind].
-    dependent destruction NC.
-    rewrite testing0 // testing0 //.
-  - intros h NC y.
+    rewrite Pr_code_put.
+    erewrite testing0.
+    + rewrite Pr_code_get E.
+      cbn [code_link].
+      repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]).
+      cbn [bind].
+      rewrite Pr_code_put.
+      reflexivity.
+    + apply SEP.
+    + apply H0.
+    + rewrite get_set_heap_eq //.
+  - intros h y.
     cbn [code_link].
     rewrite Pr_code_get.
-    dependent destruction NC.
     rewrite H1 //.
-  - intros h NC y.
+  - intros h y.
     cbn [code_link].
     rewrite Pr_code_put.
-    dependent destruction NC.
     rewrite IHVA //.
-  - intros h NC y.
+  - intros h y.
     cbn [code_link].
     rewrite Pr_code_sample.
-    dependent destruction NC.
     under eq_in_dlet.
     1: intros ? ? ?; rewrite H0 //; reflexivity.
     1: reflexivity.
@@ -185,116 +245,40 @@ Proof.
 Qed.
 
 Lemma testing {LA} {A : raw_package} :
+  fseparate LA [fmap cell] →
   LosslessOp op →
-  NCalls 1 (resolve A RUN tt) empty_heap →
   ValidPackage LA IPICK A_export A →
-  Pr (A ∘ RAND) =1 \dlet_(x <- op.π2) Pr (A ∘ PICK x).
+  Pr (A ∘ RAND) = \dlet_(x <- op.π2) Pr (A ∘ CELL ∘ PICK x).
 Proof.
-  intros LL NC VA b.
+  intros SEP LL VA.
+  apply distr_ext => b.
   unfold Pr, SDistr_bind, SDistr_unit, Pr_op.
   rewrite resolve_link.
 
   assert (H : fhas A_export RUN); [ fmap_solve |].
   pose proof (valid_resolve _ _ _ _ RUN tt VA H).
-  rewrite (testing1 LL NC H0).
+  rewrite (testing1 SEP LL H0).
   rewrite dlet_dlet.
   apply dlet_f_equal => y.
   rewrite resolve_link => //.
 Qed.
 
-Definition cell : Location := (0%N, 'option (op.π1)).
+(*
+Lemma CELL_PICK_perf x :
+  perfect IPICK (CELL ∘ PICK x) (PICK x).
+Proof.
+Admitted.
 
-Definition CELL : package IPICK IPICK :=
-  [package [fmap cell ] ;
-    [ 0%N ] : { 'unit ~> op.π1 } 'tt {
-      mc ← get cell ;;
-      match mc with
-      | Some c => ret c
-      | None => 
-        r ← call [ 0%N ] : { 'unit ~> op.π1 } tt ;;
-        #put cell := Some r ;;
-        ret r
-      end
-    } ].
-
-Lemma testings {LA} {A : raw_package} :
-  fseparate LA [fmap cell] →
+Lemma testings' {LA} {A : nom_package} :
   LosslessOp op →
   ValidPackage LA IPICK A_export A →
-  Pr (A ∘ CELL ∘ RAND) =1 \dlet_(x <- op.π2) Pr ((A ∘ CELL) ∘ PICK x).
+  Pr' (A ∘ RAND)%sep = psum (fun x => op.π2 x * Pr' (A ∘ PICK x)%sep)%R.
 Proof.
-  intros SEP LL VA x.
-  rewrite link_assoc.
-  erewrite testing => //.
-  2: ssprove_valid.
-  rewrite resolve_link.
+Admitted.
+ *)
 
-  assert (H : fhas A_export RUN); [ fmap_solve |].
-  pose proof (valid_resolve _ _ _ _ RUN tt VA H).
 
-  move: empty_heap; induction H0.
-  - intros h. apply valid_ret.
-  - intros h.
-    fmap_invert H0.
-    cbn [code_link].
-    destruct x0.
-    repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]).
-    cbn [bind].
-    apply valid_getr.
-    destruct (get_heap h cell) eqn:E.
-    { cbn [bind]. apply H2. }
-    cbn [bind].
-    apply valid_opr => r.
-    apply valid_putr.
-    clear E.
-    move: h.
-    induction (H1 r).
-    + intros h. apply valid_ret.
-    + intros h.
-      fmap_invert H0.
-      destruct x0.
-      cbn[code_link].
-      repeat (rewrite (resolve_set, resolve_link, resolve_ID_set, coerce_kleisliE, eq_refl); cbn [fst snd mkdef projT2 mkopsig projT1]).
-      cbn[bind].
-      apply valid_getr.
-      rewrite get_set_heap_eq.
-      cbn[bind].
-      apply H4.
-    + intros h.
-      cbn[code_link].
-      apply valid_getr.
-      apply H4.
-    + intros h.
-      cbn [code_link].
-      apply valid_putr.
-      rewrite set_heap_commut //.
-      apply fhas_in in H0.
-      destruct SEP as [SEP].
-      move: SEP => /fdisjointP.
-      intros H'.
-      specialize (H' _ H0).
-      rewrite domm_set domm0 in H'.
-      apply /negP.
-      move=> /eqP H''.
-      rewrite H'' in H'.
-      rewrite in_fsetU in_fset1 eq_refl // in H'.
-    + intros h.
-      cbn [code_link].
-      apply valid_sampler => y.
-      apply H3.
-  - intros h.
-    cbn [code_link].
-    apply valid_getr.
-    apply H2.
-  - intros h.
-    cbn [code_link].
-    apply valid_putr.
-    apply IHvalid_code.
-  - intros h.
-    cbn [code_link].
-    apply valid_sampler => y.
-    apply H1.
-Qed.
+  
 
 End Proof.
 
@@ -302,17 +286,18 @@ End Proof.
 Lemma testing_uni {F : finType} {LA} {A : raw_package} {b} `{Positive #|F|} :
   fseparate LA [fmap cell (uniform #|F|) ] →
   ValidPackage LA (IPICK (uniform #|F|)) A_export A →
-  Pr (A ∘ CELL (uniform #|F|) ∘ RAND (uniform #|F|)) b
+  Pr (A ∘ RAND (uniform #|F|)) b
       = (\sum_f Pr ((A ∘ CELL (uniform #|F|)) ∘ PICK (uniform #|F|) f) b / #|F|%:R)%R.
 Proof.
   intros SEP VA.
-  rewrite testings //.
+  rewrite testing //.
   rewrite dletE.
   rewrite psum_fin.
   apply eq_bigr => x _.
   rewrite GRing.mulrC.
   replace ((uniform #|F|).π2 x) with (#|F|%:R^-1 : Axioms.R)%R.
-  - apply Num.Theory.ger0_norm.
+  - rewrite link_assoc.
+    apply Num.Theory.ger0_norm.
     apply Num.Theory.mulr_ge0.
     + unfold Pr, SDistr_bind; rewrite dletE; apply ge0_psum.
     + rewrite Num.Theory.invr_ge0.
