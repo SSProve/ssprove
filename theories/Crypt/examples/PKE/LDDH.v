9261b0f1aa47a1c3dcf05e57870a380f1b6f9796
Markus Krabbe Larsen <markus@krabbelarsen.dk>
2025-12-17 17:10:32 +0100

    Reduction from LDDH to DDH.

--- theories/Crypt/examples/PKE/LDDH.v
+++ theories/Crypt/examples/PKE/LDDH.v
@@ -14,7 +14,7 @@ Set Bullet Behavior "Strict Subproofs".
 Set Default Goal Selector "!".
 Set Primitive Projections.
 
-From SSProve Require Import NominalPrelude.
+From SSProve Require Import NominalPrelude Async.
 From SSProve.Crypt.examples.PKE Require Import CyclicGroup.
 
 Import PackageNotation.
@@ -28,8 +28,8 @@ Import GroupScope.
 
 #[local] Open Scope F_scope.
 
-Section LDDH.
 
+Section LDDH.
   Context (G : CyclicGroup).
 
   Definition GETA := 50%N.
@@ -66,3 +66,146 @@ Section LDDH.
     ].
 
 End LDDH.
+
+
+Section DDH.
+  Context (G : CyclicGroup).
+
+  Definition GETABC := 52%N.
+
+  Definition I_DDH :=
+    [interface
+      [ GETABC ] : { 'unit ~> 'el G × 'el G × 'el G }
+    ].
+
+  Definition DDH bit :
+    game I_DDH :=
+    [package emptym ;
+      [ GETABC ] 'tt {
+        a ← sample uniform #|exp G| ;;
+        b ← sample uniform #|exp G| ;;
+        if bit then
+          ret ('g ^ a, 'g ^ b, ('g ^ a) ^ b)
+        else
+          c ← sample uniform #|exp G| ;;
+          ret ('g ^ a, 'g ^ b, 'g ^ c)
+      }
+    ].
+End DDH.
+
+
+Section Reduction.
+  Context (G : CyclicGroup).
+
+  Definition sample_bc bit (a : 'exp G) : dist ('el G × 'el G) := {code
+    b ← sample uniform #|exp G| ;;
+    if bit then
+      ret ('g ^ b, ('g ^ a) ^ b)
+    else
+      c ← sample uniform #|exp G| ;;
+      ret ('g ^ b, 'g ^ c)
+  }.
+
+  Definition ADDH :
+    package (I_ASYNC ('exp G) ('el G × 'el G)) (I_LDDH G) :=
+    [package emptym ;
+      [ GETA ] 'tt {
+        a ← sample uniform #|exp G| ;;
+        _ ← call [ PRIME ] a ;;
+        ret ('g ^ a)
+      } ;
+      [ GETBC ] 'tt {
+        '(gb, gc) ← call [ TRIGGER ] tt ;;
+        ret (gb, gc)
+      }
+    ].
+
+  Ltac ssprove_perfect I := 
+    ssprove_share; apply prove_perfect;
+    eapply (eq_rel_perf_ind _ _ I);
+      [ ssprove_invariant; try done |].
+
+  Lemma LDDH_ADDH b : perfect (I_LDDH G) (LDDH G b) (ADDH ∘ LAZY _ _ (sample_bc b)).
+  Proof.
+    ssprove_perfect (heap_ignore [fmap mga_loc G ; lazy_loc ('exp G)]
+      ⋊ couple_cross (mga_loc G) (lazy_loc ('exp G)) (λ mga ma, mga = omap (λ a, 'g ^ a) ma)).
+    simplify_eq_rel arg.
+    - destruct arg. simpl.
+      ssprove_code_simpl.
+      ssprove_sync => a.
+      apply r_put_vs_put.
+      ssprove_restore_pre. { ssprove_invariant. }
+      by apply r_ret.
+    - destruct arg. simpl.
+      ssprove_code_simpl; simpl.
+      apply r_get_remember_lhs => mga.
+      apply r_get_remember_rhs => ma.
+      ssprove_code_simpl_more.
+      ssprove_rem_rel 0%N.
+      destruct mga, ma => //= Heq; [| apply r_fail ].
+      noconf Heq.
+      apply r_put_vs_put.
+      ssprove_sync => b'.
+      ssprove_restore_mem. { ssprove_invariant. }
+      destruct b => /=.
+      + by apply r_ret.
+      + ssprove_sync => c.
+        by apply r_ret.
+  Qed.
+
+  Definition mgbc_loc := mkloc 52%N (None : 'option ('el G × 'el G)).
+
+  Definition RDDH :
+    package (I_DDH G) (I_LDDH G) :=
+    [package [fmap mgbc_loc ];
+      [ GETA ] 'tt {
+        '(ga, gb, gc) ← call [ GETABC ] tt ;;
+        #put mgbc_loc := Some (gb, gc) ;;
+        ret ga
+      } ;
+      [ GETBC ] 'tt {
+        gbc ← getSome mgbc_loc ;;
+        #put mgbc_loc := None ;; 
+        ret gbc
+      }
+    ].
+
+  Lemma ADDH_RDDH b : perfect (I_LDDH G) (ADDH ∘ EAGER _ _ (sample_bc b)) (RDDH ∘ DDH G b).
+  Proof.
+    ssprove_perfect (heap_ignore [fmap mgbc_loc ; eager_loc ('el G × 'el G) ]
+      ⋊ couple_cross (eager_loc ('el G × 'el G)) mgbc_loc eq).
+    simplify_eq_rel arg.
+    - destruct arg. simpl.
+      ssprove_code_simpl; simpl.
+      ssprove_sync => a.
+      ssprove_sync => b'.
+      destruct b => /=.
+      + apply r_put_vs_put.
+        ssprove_restore_pre. { ssprove_invariant. }
+        by apply r_ret.
+      + ssprove_sync => c.
+        apply r_put_vs_put.
+        ssprove_restore_pre. { ssprove_invariant. }
+        by apply r_ret.
+    - destruct arg. simpl.
+      ssprove_code_simpl; simpl.
+      apply r_get_remember_lhs => lhs.
+      apply r_get_remember_rhs => mgbc.
+      ssprove_rem_rel 0%N => {lhs}->.
+      ssprove_code_simpl_more.
+      ssprove_sync => Hmgbc.
+      destruct mgbc as [ [gb gc] |] => //=.
+      apply r_put_vs_put.
+      ssprove_restore_mem. { ssprove_invariant. }
+      by apply r_ret.
+  Qed.
+
+  Lemma LDDH_DDH {A} {VA : ValidPackage (loc A) (I_LDDH G) A_export A} :
+    AdvOf (LDDH G) A = AdvOf (DDH G) (A ∘ RDDH).
+  Proof.
+    rewrite (AdvOf_perfect LDDH_ADDH).
+    rewrite Adv_reduction -(Adv_perfect_l (ASYNC_perfect _ _ _ _)).
+    rewrite -(Adv_perfect_r (ASYNC_perfect _ _ _ _)).
+    by rewrite -Adv_reduction (AdvOf_perfect ADDH_RDDH) Adv_reduction.
+  Qed.
+End Reduction.
---
+++ theories/Crypt/nominal/packages/Async.v
@@ -0,0 +1,259 @@
+From Coq Require Import Utf8.
+Set Warnings "-ambiguous-paths,-notation-overridden,-notation-incompatible-format".
+From mathcomp Require Import all_ssreflect all_algebra reals distr realsum.
+Set Warnings "ambiguous-paths,notation-overridden,notation-incompatible-format".
+From extructures Require Import ord fset fmap ffun fperm.
+Require Import Equations.Prop.DepElim.
+From Equations Require Import Equations.
+
+From SSProve.Crypt Require Import Axioms SubDistr pkg_composition
+  Prelude Package Nominal NominalPrelude.
+
+From HB Require Import structures.
+
+(* Supress warnings due to use of HB *)
+Set Warnings "-redundant-canonical-projection,-projection-no-head-constant".
+
+Set Bullet Behavior "Strict Subproofs".
+Set Default Goal Selector "!".
+Set Primitive Projections.
+
+Import PackageNotation.
+#[local] Open Scope package_scope.
+
+Section Async.
+  Definition PRIME := 55%N.
+  Definition TRIGGER := 56%N.
+
+  Context (T' T : choice_type).
+  Context (c : T' → dist T).
+  Context (LL : ∀ t, LosslessCode (c t)).
+
+  Definition I_ASYNC :=
+    [interface
+      [ PRIME  ] : { T' ~> 'unit } ;
+      [ TRIGGER ] : { 'unit ~> T }
+    ].
+
+  Definition eager_loc : Location := mkloc 53 (None : option T).
+  Definition lazy_loc : Location := mkloc 53 (None : option T').
+
+  Definition EAGER :
+    game I_ASYNC :=
+    [package [fmap eager_loc ] ;
+      [ PRIME ] : { T' ~> 'unit } (t) {
+        r ← c t ;;
+        #put eager_loc := Some r ;;
+        ret tt
+      } ;
+      [ TRIGGER ] : { 'unit ~> T } 'tt {
+        r ← getSome eager_loc ;;
+        #put eager_loc := None ;;
+        ret r
+      }
+    ].
+
+  Definition LAZY :
+    game I_ASYNC :=
+    [package [fmap lazy_loc ] ;
+      [ PRIME ] : { T' ~> 'unit } (t) {
+        #put lazy_loc := Some t ;;
+        ret tt
+      } ;
+      [ TRIGGER ] : { 'unit ~> T } 'tt {
+        t ← getSome lazy_loc ;;
+        #put lazy_loc := None ;;
+        r ← c t ;;
+        ret r
+      }
+    ].
+
+  Definition ASYNC b := if b then EAGER else LAZY.
+
+  Lemma neq_fst {L L' : Locations} {l l' : Location}
+    : fseparate L L' → fhas L l → fhas L' l' → l.1 != l'.1.
+  Proof.
+    move=> HSEP HL HL'.
+    move: (notin_has_separate _ _ _ HL HSEP) => /negP Hdomm.
+    apply /eqP => Heq. apply Hdomm. rewrite Heq. by apply fhas_in.
+  Qed.
+
+  Definition tape {U} : option T' → (option T → distr R U) → distr R U := λ o f,
+    match o with
+    | None => f None
+    | Some t => \dlet_(x <- Pr_fst (c t)) f (Some x)
+    end.
+
+  Lemma set_set h v1 v2 :
+    set_heap (set_heap h lazy_loc v1) eager_loc v2 = set_heap h eager_loc v2.
+  Proof.
+    rewrite /set_heap. apply eq_fmap => x. rewrite 3!setmE.
+    simpl. by destruct (x == 53%N)%B.
+  Qed.
+
+  Lemma ASYNC_0 LA (A : raw_code bool) h :
+    ValidCode LA I_ASYNC A →
+    fseparate LA [fmap eager_loc] →
+    fseparate LA [fmap lazy_loc] →
+    dfst (tape (get_heap h lazy_loc) (λ y, Pr_code (code_link A EAGER) (set_heap h eager_loc y)))
+      = dfst (Pr_code (code_link A LAZY) h).
+  Proof.
+    intros VA SEP1 SEP2.
+    move: h; induction VA => h; cbn [code_link].
+    - destruct (get_heap h lazy_loc) => /=.
+      + rewrite Pr_code_ret 2!dmarginE dlet_unit_ext /=.
+        rewrite dlet_dlet_ext.
+        under eq_dlet. { intros ?. rewrite Pr_code_ret dlet_unit_ext /=. over. }
+        by rewrite Pr_fstC.
+      + by rewrite 2!Pr_code_ret 2!dmarginE 2!dlet_unit_ext.
+    - rewrite Pr_code_bind.
+      fmap_invert H.
+      + simplify_linking.
+        destruct (get_heap h lazy_loc) => /=.
+        * rewrite bind_assoc.
+          under eq_dlet. {
+            intros ?. rewrite Pr_code_rand.
+            under eq_dlet. {
+              intros ?. rewrite Pr_code_put set_heap_contract. over. }
+            over. }
+          rewrite /= Pr_fstC. 
+          rewrite Pr_code_put Pr_code_ret dlet_unit_ext /=.
+          specialize (H1 tt (set_heap h lazy_loc (Some x))).
+          rewrite -H1 get_set_heap_eq /=.
+          f_equal. symmetry.
+          apply eq_dlet => ?. by rewrite set_set.
+        * rewrite bind_assoc Pr_code_rand.
+          under eq_dlet. {
+            intros ?. rewrite Pr_code_put set_heap_contract.
+            over. }
+          rewrite /= Pr_code_put Pr_code_ret dlet_unit_ext /=.
+          specialize (H1 tt (set_heap h lazy_loc (Some x))).
+          rewrite -H1 get_set_heap_eq /=.
+          f_equal. apply eq_dlet => ?. by rewrite set_set.
+      + simplify_linking.
+        destruct x.
+        rewrite Pr_code_get.
+        destruct (get_heap h lazy_loc) => /=.
+        * under eq_dlet. { intros ?.
+            rewrite Pr_code_get get_set_heap_eq /=.
+            rewrite Pr_code_put set_heap_contract. over. }
+          rewrite Pr_code_put Pr_code_rand. symmetry.
+          rewrite dlet_dlet_ext.
+          under eq_dlet. { intros ?.
+            rewrite Pr_code_ret dlet_unit_ext /=. over. }
+          rewrite 2!dmarginE dlet_dlet_ext. symmetry.
+          rewrite dlet_dlet_ext. apply eq_dlet => t.
+          specialize (H1 t (set_heap h lazy_loc None)).
+          rewrite 2!dmarginE get_set_heap_eq /= in H1.
+          by rewrite -H1 set_set.
+        * rewrite Pr_code_get get_set_heap_eq /=.
+          rewrite Pr_code_sample /= dlet_null_ext.
+          by rewrite Pr_code_fail dlet_null_ext.
+    - rewrite Pr_code_get -H1.
+      epose proof (neq_fst SEP2 H).
+      assert (l.1 != lazy_loc.1). { apply H2. fmap_solve. }
+      destruct (get_heap h lazy_loc); cbn [tape].
+      + under eq_dlet. { intros ?.
+          rewrite Pr_code_get.
+          rewrite get_set_heap_neq //.
+          over. }
+        done.
+      + rewrite Pr_code_get.
+        by rewrite get_set_heap_neq.
+    - rewrite Pr_code_put -IHVA.
+      epose proof (neq_fst SEP2 H).
+      assert (lazy_loc.1 != l.1). { rewrite eq_sym. apply H0. fmap_solve. }
+      rewrite get_set_heap_neq //.
+      destruct (get_heap h lazy_loc); cbn [tape].
+      + under eq_dlet. { intros ?. rewrite Pr_code_put set_heap_commut //. over. }
+        done.
+      + by rewrite Pr_code_put set_heap_commut.
+    - rewrite Pr_code_sample.
+      destruct (get_heap h lazy_loc) eqn:E; cbn [tape].
+      + rewrite 2!dfst_dlet_commut.
+        under eq_dlet. { intros ?. rewrite Pr_code_sample dfst_dlet_commut. over. }
+        rewrite dlet_commut. apply eq_dlet => v.
+        specialize (H0 v h). rewrite -H0. rewrite E. cbn [tape].
+        by rewrite dfst_dlet_commut.
+      + rewrite Pr_code_sample 2!dfst_dlet_commut.
+        apply eq_dlet => v.
+        specialize (H0 v h). rewrite -H0 E.
+        by cbn [tape].
+  Qed.
+
+  Lemma ASYNC_0_Adv LA (A : raw_package) :
+    ValidPackage LA I_ASYNC A_export A →
+    fseparate LA [fmap eager_loc] →
+    fseparate LA [fmap lazy_loc] →
+    Pr (A ∘ EAGER) true = Pr (A ∘ LAZY) true.
+  Proof.
+    intros VA SEP1 SEP2.
+    rewrite 2!Pr_Pr_code 2!resolve_link.
+    assert (H : fhas A_export RUN); [ fmap_solve |].
+    pose proof (valid_resolve _ _ _ _ RUN tt VA H).
+    rewrite -(ASYNC_0 _ _ empty_heap H0 SEP1 SEP2).
+    rewrite get_empty_heap /heap_init /lazy_loc. cbn [mkloc snd projT2 tape].
+
+    f_equal.
+    assert (HNone : get_heap empty_heap eager_loc = None).
+    { by rewrite get_empty_heap. }
+    move: empty_heap HNone.
+    induction H0 => h HNone; cbn [code_link].
+    - by rewrite 2!Pr_code_ret 2!dmarginE 2!dlet_unit_ext.
+    - rewrite Pr_code_bind.
+      fmap_invert H0.
+      + simplify_linking.
+        rewrite Pr_code_rand dlet_dlet_ext. symmetry.
+        rewrite bind_assoc Pr_code_rand.
+        rewrite 2!dfst_dlet_commut. apply eq_dlet => x'.
+        rewrite Pr_code_put. symmetry. rewrite Pr_code_put Pr_code_ret.
+        by rewrite dlet_unit_ext set_heap_contract.
+      + simplify_linking.
+        destruct x.
+        rewrite Pr_code_get HNone /= Pr_code_fail dlet_null_ext.
+        rewrite Pr_code_get get_set_heap_eq Pr_code_sample /=.
+        by rewrite dlet_null_ext.
+    - epose proof (neq_fst SEP2 H0).
+      assert (l.1 != lazy_loc.1). { apply H3. fmap_solve. }
+      rewrite 2!Pr_code_get get_set_heap_neq; auto.
+    - rewrite 2!Pr_code_put.
+      epose proof (neq_fst SEP1 H0).
+      assert (eager_loc.1 != l.1). { rewrite eq_sym. apply H2. fmap_solve. }
+      rewrite set_heap_commut; auto.
+      apply IHvalid_code.
+      by rewrite get_set_heap_neq.
+    - rewrite 2!Pr_code_sample 2!dfst_dlet_commut. apply eq_dlet.
+      intros ?. by apply H1.
+  Qed.
+
+  Lemma Adv_nom_ind (G G' A : nom_package) I {P : R → Type} :
+    (∀ (A' : raw_package) LA, ValidPackage LA I A_export A' →
+      val A ≡ A' →
+      fseparate LA (loc G) →
+      fseparate LA (loc G') →
+      P (AdvantageE G G' A')
+    ) →
+    ValidPackage (loc A) I A_export A →
+    P (Adv G G' A).
+  Proof.
+    intros HP VA.
+    pose (π := fresh ((G, loc G), (G', loc G')) (loc A, A)).
+    replace (Adv G G' A) with (AdvantageE G G' (π ∙ A : nom_package)).
+    2: rewrite -{2}(@rename_alpha _ A π) //.
+    2: rewrite /Adv {1}/Pr' -link_sep_link.
+    3: eauto with nominal_db.
+    2: rewrite {1}/Pr' -link_sep_link //.
+    2: eauto with nominal_db.
+    eapply HP.
+    1: eapply rename_valid; apply VA.
+    1: symmetry; apply rename_alpha.
+    1,2: rewrite fseparate_disj ; eauto with nominal_db.
+  Qed.
+
+  Lemma ASYNC_perfect : perfect (I_ASYNC) EAGER LAZY.
+  Proof.
+    intros A VA. eapply Adv_nom_ind; [| apply VA].
+    intros A' LA VA' _ SEP1 SEP2.
+    by rewrite /AdvantageE ASYNC_0_Adv // GRing.subrr Num.Theory.normr0.
+  Qed.
+End Async.
