14649105154c39048f310dd1309b9ba1f0f58217
Vincent Laporte <Vincent.Laporte@inria.fr>
2026-03-06 22:39:01 +0100

    Update mathcomp-word to 3.3

--- .nix/nixpkgs.nix
+++ .nix/nixpkgs.nix
@@ -1,4 +1,4 @@
 fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/62dab73c6aa5a9da858ef26d29600541c85eed6d.tar.gz";
-    sha256 = "1mj9v5s2129h6ffg0awc9b39mh3l41nqm0mpnyvn8idmhxja8wbq";
-  }
+         url = "https://github.com/vbgl/nixpkgs/archive/128bccf79b6e005d91bd7ad6b99ce97f6f220726.tar.gz";
+         sha256 = "1l9gddkrs103fg9qc88x18lz67xsrcs92d2fqk830jhmsrgbb26l";
+       }
