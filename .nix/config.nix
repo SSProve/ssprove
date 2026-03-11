61a267288430783607567bf8783c3731468d3e14
4ever2 <3417013+4ever2@users.noreply.github.com>
2026-03-11 15:37:15 +0100

    Drop 8.20 support

--- .nix/config.nix
+++ .nix/config.nix
@@ -1,56 +1,12 @@
 {
   ## DO NOT CHANGE THIS
   format = "1.0.0";
-  ## unless you made an automated or manual update
-  ## to another supported format.
 
-  ## The attribute to build from the local sources,
-  ## either using nixpkgs data or the overlays located in `.nix/coq-overlays`
-  ## Will determine the default main-job of the bundles defined below
   attribute = "ssprove";
 
-  ## If you want to select a different attribute (to build from the local sources as well)
-  ## when calling `nix-shell` and `nix-build` without the `--argstr job` argument
-  # shell-attribute = "{{nix_name}}";
-
-  ## Maybe the shortname of the library is different from
-  ## the name of the nixpkgs attribute, if so, set it here:
-  # pname = "{{shortname}}";
-
-  ## Lists the dependencies, phrased in terms of nix attributes.
-  ## No need to list Coq, it is already included.
-  ## These dependencies will systematically be added to the currently
-  ## known dependencies, if any more than Coq.
-  ## /!\ Remove this field as soon as the package is available on nixpkgs.
-  ## /!\ Manual overlays in `.nix/coq-overlays` should be preferred then.
-  # buildInputs = [ ];
-
-  ## Indicate the relative location of your _CoqProject
-  ## If not specified, it defaults to "_CoqProject"
-  # coqproject = "_CoqProject";
-
   no-rocq-yet = true;
+  default-bundle = "9.0";
 
-  ## select an entry to build in the following `bundles` set
-  ## defaults to "default"
-  default-bundle = "8.20";
-
-  ## write one `bundles.name` attribute set per
-  ## alternative configuration
-  ## When generating GitHub Action CI, one workflow file
-  ## will be created per bundle
-
-  ## It is not possible as of yet to the coq-nix-toolbox to use a
-  ## certain version. All jobs only run against the latest version.
-  ## To do this check as a CI job you either need to manually edit
-  ## the generated workflow or create branch off of version 0.1.0
-  ## and add it there.
-  bundles."8.20".coqPackages = {
-    coq.override.version = "8.20";
-    mathcomp.job = false;
-    mathcomp.override.version = "2.2.0";
-    mathcomp-analysis.override.version = "1.2.0";
-  };
   bundles."9.0" = { coqPackages = {
       coq.override.version = "9.0";
       mathcomp.job = false;
@@ -87,28 +43,11 @@
     mathcomp-bigenough.override.version = "master";
   };
 
-  bundles."8.20".push-branches = ["main"];
   bundles."9.0".push-branches = ["main"];
   bundles."9.1".push-branches = ["main"];
 
-  ## Cachix caches to use in CI
-  ## Below we list some standard ones
   cachix.coq = {};
   cachix.math-comp = {};
   cachix.coq-community = {};
-
-  ## If you have write access to one of these caches you can
-  ## provide the auth token or signing key through a secret
-  ## variable on GitHub. Then, you should give the variable
-  ## name here. For instance, coq-community projects can use
-  ## the following line instead of the one above:
-  # cachix.coq-community.authToken = "CACHIX_AUTH_TOKEN";
   cachix.ssprove.authToken = "CACHIX_AUTH_TOKEN";
-
-  ## Or if you have a signing key for a given Cachix cache:
-  # cachix.my-cache.signingKey = "CACHIX_SIGNING_KEY"
-
-  ## Note that here, CACHIX_AUTH_TOKEN and CACHIX_SIGNING_KEY
-  ## are the names of secret variables. They are set in
-  ## GitHub's web interface.
 }
